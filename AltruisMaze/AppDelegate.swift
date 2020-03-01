a826dea59e4796fcb8b36e4d1123fc440a19d4fe
Shawn Bomar <shooterq64@gmail.com>
2020-03-01 17:41:56 -0500

    Splash Sketch updated

--- AltruisMaze/AppDelegate.swift
+++ AltruisMaze/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        Thread.sleep(forTimeInterval: 1.25)
+        Thread.sleep(forTimeInterval: 1.65)
         FirebaseApp.configure()
         return true
     }
